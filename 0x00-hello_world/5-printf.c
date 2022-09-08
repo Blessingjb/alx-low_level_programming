22756809e97cf602ed01804478ac6b67d1741c78
Blessingjb <blessingbunmi95@gmail.com>
2022-09-08 06:50:54 -0700

    05

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,11 +4,9 @@
 
 /**
 
- * main - prints the phrase with proper grammar,
+ * main - Entry point
 
- * but the outcome is a piece of art,
-
- * Return: 0 if exited properly, non-zero otherwise
+ * Return: Always 0 (Success)
 
  */
 
@@ -21,3 +19,5 @@ printf("with proper grammar, but the outcome is a piece of art,\n");
 return (0);
 
 }
+
+
