6f804f875fc14f0be9fa2e2d47d155a48129c1dc
Blessingjb <blessingbunmi95@gmail.com>
2022-09-26 11:39:27 -0700

    00

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <string.h>
 /**
 * _memset -> this memory set function
 * @s: string
@@ -17,9 +16,9 @@ char *_memset(char *s, char b, unsigned int n)
 
 
 	for (a = 0; a < n; a++)
-
+	{
 		s[a] = b;
-
+	}
 
 
 	return (s);
